b365c7f84822314e4512b357c1d7198d7a7569c7
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-06-30 11:37:33 -0700

    update Harshith js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,21 @@ $(function (){
         $("#due-date").val("")
         printProject(projName, projType, hourlyWage, dueDate)
     })
+    function daysTill(projDue){
+        return (moment(projDue).dayOfYear() - moment().dayOfYear)
+    }
+    function projRev(projDue, wage){
+        return (daysTill(projDue) * 8 * wage)
+    }
     function printProject(pname, ptype, hwage, date){
         var tableRow = $("<tr>")
         var nameTd = $("<td>").text(pname)
         var typeTd = $("<td>").text(ptype)
         var wageTd = $("<td>").text(hwage)
         var dateTd = $("<td>").text(date)
-        tableRow.append(nameTd, typeTd, wageTd, dateTd)
-        $("thead").append(tableRow)
+        var daysTillTd = $("<td>").text(daysTill(date))
+        var revTd = $("<td>").text(projRev(date, hwage))
+        tableRow.append(nameTd, typeTd, wageTd, dateTd, daysTillTd, revTd)
+        $("#project-table").append(tableRow)
     }
 })
